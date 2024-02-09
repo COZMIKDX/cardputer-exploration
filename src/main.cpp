d73caa2e114114947bea066eea38c639eacf9d45
CosmicTacoCat <tacocatdx@gmail.com>
2024-02-09 16:10:49 -0600

    Add comments explaining where things should be called and some thoughts.

--- src/main.cpp
+++ src/main.cpp
@@ -24,9 +24,24 @@ void setup(void)
     auto cfg = M5.config();
     M5Cardputer.begin(cfg);
    
+   /*
+    Put your setup function calls here.
+    The current general structure I chose is for called called in this file to have a setup function placed here
+    and some sort of run function that is to be placed in the loop function.
+    I'm open to running functions that do more than set things up here in setup() but I don't want them infinitely looping here.
+   */
 }
 
 void loop(void)
 {
-
+    /*
+    Put your run function calls here.
+    If you would put it in this loop() function in a normal arduino project, put it here.
+    Just don't put infinite or long while loops in them.
+    I would like this to be able to call several functions If I need to. For example, one app function could be simply placing 
+    a clock at the top of the screen and another would be my tone generator app. The result would be my tone generator app but 
+    with a clock at the top.
+    I suppose this could be a sort of app builder or more like feature compositor. 
+    I don't want to get too far into using it like that without thinking it over and making a written plan first.
+    */
 }
