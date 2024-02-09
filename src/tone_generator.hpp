a3fd00c6d0ab75f7a39a6472ffaa0a57f9fedff9
CosmicTacoCat <tacocatdx@gmail.com>
2024-02-09 14:31:36 -0600

    Create tone_generator header

---
+++ src/tone_generator.hpp
@@ -0,0 +1,10 @@
+#include <M5Cardputer.h>
+
+void tone_generator_setup(m5::M5_CARDPUTER M5Cardputer);
+void run_tone_generator(m5::M5_CARDPUTER M5Cardputer);
+void volume_check();
+void frequency_up();
+void frequency_down();
+void master_volume_up();
+void master_volume_down();
+void drawStaticUI();
